93a454e8a4e1991888677a666c7b70ac0b1dfc7f
rsr986 <rsr986@gmail.com>
2018-11-15 14:13:13 +0530

    Added merge sort

--- algos/sort/main.c
+++ algos/sort/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "sort.h"
+//#include "merge.h"
 
 void printArr(unsigned int* arr, unsigned int len);
 
@@ -7,9 +8,8 @@ void main()
 {
 	unsigned int arr[] = {90, 39, 89, 34, 52, 6, 23, 1, 646, 2};
 	printArr(arr, 10);
-//	bubbleSort(arr, 10);
-	insertionSort(arr, 10);
-//	selectionSort(arr, 10);
+	
+	mergeSort(arr, 0, 10);
 	printArr(arr, 10);
 }
 
--- algos/sort/sort.h
+++ algos/sort/sort.h
@@ -5,6 +5,78 @@ void bubbleSort(unsigned int* arr, unsigned int len);
 void selectionSort(unsigned int* arr, unsigned int len);
 void insertionSort(unsigned int* arr, unsigned int len);
 
+static void printArray(int a[], unsigned int size);
+static void mergeTwoArrays(int a[], int size, int a1[], int size1,
+	int a2[], int size2);
+
+void mergeSort(int a[], unsigned int start, unsigned int end)
+{
+	int i, j, middle = (start + end)/2;
+	int a1[middle], a2[end-middle];
+
+	if(end == 1)
+		return;
+
+	for(i=0; i < middle; i++)
+		a1[i]=a[i];
+	for(j=0; j+i < end; j++)
+	       a2[j]=a[j+i];
+
+//	printArray(a1, middle);
+//	printArray(a2, end-middle);
+
+	mergeSort(a1, start, middle);
+	mergeSort(a2, 0, end-middle);
+
+	mergeTwoArrays(a, end, a1, middle, a2, end-middle);
+}
+
+static void mergeTwoArrays(int a[], int size, int a1[], int size1,
+	int a2[], int size2)
+{
+	int i=0, j=0, k=0;
+	for(i=0, j=0, k=0; i<size1 && j<size2; k++)
+	{
+		if(a1[i] <= a2[j])
+		{
+			a[k] = a1[i];
+			i++;
+		}	
+		else
+		{
+			a[k] = a2[j];
+			j++;
+		}
+	}
+
+	if(i < size1)
+	{
+		while(i < size1)
+		{
+			a[k] = a1[i];
+			i++;
+			k++;
+		}
+	}
+
+	if(j < size2)
+	{
+		while(j < size2)
+		{
+			a[k] = a2[j];
+			j++;
+			k++;
+		}
+	}
+
+}
+
+static void printArray(int a[], unsigned int size)
+{
+	for(int i=0; i<size; i++)
+		printf("%d ", a[i]);
+	printf("\n");
+}
 
 void insertionSort(unsigned int* arr, unsigned int len)
 {
